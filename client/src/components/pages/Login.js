7aea90d97a82fd0479632cc4f75fc9a2d8b5436e
Alex Ullmann <aullmann3@gmail.com>
2022-11-17 19:01:28 +0100

    searchbar complete

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,8 +1,16 @@
 const Login = () => {
   return (
-    <main className='login'>
-      <h1> Login</h1>
-    </main>
+    <>
+      <div className='login form-container'>
+        <h1>Welcome Back!</h1>
+        <div className=' flex-center'></div>
+        <form className='form' action='#' method='post'>
+          <input type='email' placeholder='Username' required />
+          <input type='password' placeholder='Password' required />
+        </form>
+        <button id='signup-btn'>Log in</button>
+      </div>
+    </>
   )
 }
 export default Login
